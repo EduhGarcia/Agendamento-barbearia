b41a7a5f4452cbdb84544141018909105eeda629
Eduardo Garcia <eg836151@gmail.com>
2023-12-20 22:38:48 -0300

    Criaçao de funçoes globais

--- FrontEnd/src/scripts/utilizadades.js
+++ FrontEnd/src/scripts/utilizadades.js
@@ -3,34 +3,35 @@ const main = $('main')
 
 let next = 0
 
-export function checkInputs() {
+export function nextPage() {
     const inputs = $('*.input-login')
-
-    if (next === 2) {
-        inputs.each(index => {
-            inputs[index].value = ''
-        })
-
-        closeScreenConnect()
-        return next = 0
-    }
-
+    const labelError = $('*.input-error')
     next = 0
 
     inputs.each((index, item) => {
-        item.addEventListener('change', () => indentifyInputError(item, index))
-
-        indentifyInputError(item, index)
+        item.addEventListener('change', () => indentifyInputError(item, labelError[index]))
+        indentifyInputError(item, labelError[index])
     })
+
+    if (next === inputs.length) {
+        if (inputs.length === 3 && inputs[1].value.length < 6) {
+            labelError[1].classList.remove('display-disable')
+            labelError[1].innerText = 'a senha deve ter no minimo 6 digitos'
+        } else {
+            clearValueInput()
+            window.location.href = './usuario.html'
+            next = 0
+            return
+        }
+        
+    }
 }
 
-export function indentifyInputError(item, index) {
-    const labelError = $('*.input-error')
-
+export function indentifyInputError(item, labelError) {
     if (item.value === '') {
-        labelError[index].classList.remove('display-disable')
+        labelError.classList.remove('display-disable')
     } else {
-        labelError[index].classList.add('display-disable')
+        labelError.classList.add('display-disable')
         next++
     }
 }
@@ -45,4 +46,22 @@ export function openScreenConnect(functionButton) {
 export function closeScreenConnect() {
     connecting.addClass('display-disable')
     main.css("filter", "blur(0)")
+
+    removeLabelError()
+}
+
+function removeLabelError() {
+    const labelError = $('*.input-error')
+
+    labelError.each((index, item) => {
+        item.classList.add('display-disable')
+    })
+
+    clearValueInput()
+}
+
+function clearValueInput() {
+    $('*.input-login').each((i, element) => {
+        element.value = ''
+    })
 }
