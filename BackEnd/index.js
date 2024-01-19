e51ed1613e55cc73276bb02f1da2be7c23bb200c
Eduardo Garcia <eg836151@gmail.com>
2024-01-19 00:05:18 -0300

    debug

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,6 +10,14 @@ const prisma = new PrismaClient()
 app.use(cors("https://barbearia-agendamentos-7z52.onrender.com"))
 app.use(express.json())
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://example.com'); // Replace with your domain
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    app.use(cors({origin: "https://barbearia-agendamentos-7z52.onrender.com"}))
+    next();
+  });
+
 app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocument))
 
 let userInfo = {
