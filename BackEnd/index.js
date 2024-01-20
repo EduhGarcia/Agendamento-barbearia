72cf41084a43668b126ab7492c2925e6ec66633d
Eduardo Garcia <eg836151@gmail.com>
2024-01-20 13:34:28 -0300

    ajustando access-control-allow-origin

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', ['Content-Type', 'access-control-allow-origin']);
     next();
   });
 
