4ad2804f85e53192ef2af7969f163514ccae7b80
Eduardo Garcia <eg836151@gmail.com>
2024-01-20 15:01:21 -0300

    modificação no middleware

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,13 +9,17 @@ const prisma = new PrismaClient()
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin');
-    next();
-  });
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin');
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocument))
 
