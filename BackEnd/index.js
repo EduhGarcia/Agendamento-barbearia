ff709028771857e8fadcdaa64608a367a13b0f28
Eduardo Garcia <eg836151@gmail.com>
2024-01-20 09:30:48 -0300

    debug

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://barbearia-agendamentos-7z52.onrender.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
