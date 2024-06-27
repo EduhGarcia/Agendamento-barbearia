13577a3dc38a64ec7e588cab6038c8b6cb59f9f1
Eduardo Garcia <eg836151@gmail.com>
2024-06-27 18:05:27 -0300

    Modificação cabeçalho

--- BackEnd/src/scripts/server.js
+++ BackEnd/src/scripts/server.js
@@ -8,7 +8,7 @@ const prisma = new PrismaClient()
 app.use(express.json())
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://agender-barbearia.s3-website.us-east-2.amazonaws.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin');
