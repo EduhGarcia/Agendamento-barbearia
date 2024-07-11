83a9d1b954701aa0cdb3f0f997c4301c3edded46
Eduardo Garcia <eg836151@gmail.com>
2024-07-11 17:38:14 -0300

    Subindo server para a netlify

--- BackEnd/src/scripts/server.js
+++ BackEnd/src/scripts/server.js
@@ -3,11 +3,10 @@ const dotenv = require("dotenv")
 const { PrismaClient } = require("@prisma/client")
 const cors = require("cors")
 const SwaggerUi = require("swagger-ui-express")
-const swaggerDocument = require("./gerenciamento-barbearia.json")
+const swaggerDocument = require("../../gerenciamento-barbearia.json")
 
 const app = express()
 const prisma = new PrismaClient()
-const PORT = process.env.PORT || 3000 
 
 dotenv.config()
 app.use(cors())
@@ -150,4 +149,4 @@ app.get('/message', function (req, res) {
     userInfo.alertAnnimation = ''
 })
 
-app.listen(PORT, () => console.log("Servidor iniciado"))
+module.exports = app
