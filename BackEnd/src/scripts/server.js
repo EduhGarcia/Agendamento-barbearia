a7fa894ee4d6678303ff2086e7d7f62dcf74042d
Eduardo Garcia <eg836151@gmail.com>
2024-07-12 20:17:29 -0300

    privando Access-Control-Allow-Origin

--- BackEnd/src/scripts/server.js
+++ BackEnd/src/scripts/server.js
@@ -8,7 +8,7 @@ const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://agendamento-barbearia-4wr4i5d4o-eduhgarcias-projects.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin');
